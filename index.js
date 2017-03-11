289a6eb3ed45582e790f8e21400bc446743d8c16
fooerizer <fooeriz@e.rs>
2017-03-11 04:44:19 +0200

    b

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const app = new Koa();
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const supertest = require('supertest')
+const app = new Koa()
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
 
-var Cat = mongoose.model('Cat', { name: String });
+var Cat = mongoose.model('Cat', { name: String })
 
 app.use(bodyParser())
 
 app.use(async ctx => {
-  var kitty = new Cat({ name: ctx.request.body['name'] });
+  var kitty = new Cat({ name: ctx.request.body['name'] })
   ctx.body = await kitty.save()
-});
+})
 
-app.listen(3000);
+
+
+let request = supertest(app.listen())
+request.post('/')
+  .field('name', 'foo')
+  .expect(200, function(err, res) {
+    if (err) throw err
+    console.log('k')
+    process.exit()
+  })
